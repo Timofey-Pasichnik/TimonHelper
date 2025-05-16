3d852a49867143fc3dba21adaaf49afd534b3da7
Timon <timmofonnius@gmail.com>
2025-05-16 21:16:56 +0300

    TH-11 tried to implement aoe counter. unsuccessfully

--- TimonHelper/Modules/Spells/Common.lua
+++ TimonHelper/Modules/Spells/Common.lua
@@ -1,5 +1,8 @@
 print('Common.lua loaded')
 
+targets = {}
+targets_counter = 0
+
 common_spells = {
     attack = {
         action_slot = 25,
@@ -34,12 +37,45 @@ end
 local raw_combat_log = CreateFrame("Frame")
 raw_combat_log:RegisterEvent("RAW_COMBATLOG")
 raw_combat_log:SetScript("OnEvent", function()
-    --print(arg1)
-    if arg1 == 'CHAT_MSG_COMBAT_CREATURE_VS_SELF_HITS' then
-        print('kek')
-        hexValue = string.match(arg2, "^0x%x+")
-        print(hexValue)
+    print('counter is: ' .. targets_counter)
+    for guid in pairs(targets) do
+        local counter = 0
+        --print(guid)
+        if UnitExists(guid) == 0 or UnitIsDead(guid) == 1 then
+            targets[guid] = nil
+        else
+            counter = counter + 1
+            targets_counter = counter
+            --else
+            --    targets[guid] = nil
+        end
     end
+    if arg1 == 'CHAT_MSG_COMBAT_CREATURE_VS_SELF_HITS' or arg1 == 'CHAT_MSG_COMBAT_CREATURE_VS_SELF_HITS' then
+        local start_pos, end_pos = string.find(arg2, "^0x[0-9A-Fa-f]+")
+        if start_pos then
+            local guid = string.sub(arg2, start_pos, end_pos)
+            targets[guid] = GetTime()
+            --print(guid .. 'added')
+        end
+    --    else if arg1 == 'CHAT_MSG_COMBAT_HOSTILE_DEATH' then
+    --    --print(arg2)
+    --    local start_pos, end_pos = string.find(arg2, "0x[0-9A-Fa-f]+")
+    --    if start_pos then
+    --        local guid = string.sub(arg2, start_pos, end_pos)
+    --        targets[guid] = nil
+    --        --print(guid .. 'is dead')
+    --    end
+    --end
+    end
+    --    print('kek')
+    --    print('arg1 ' .. arg1)
+    --    print('arg2 ' .. arg2)
+    --    local start_pos, end_pos = string.find(arg2, "^0x[0-9A-Fa-f]+")
+    --    if start_pos then
+    --        local hexValue = string.sub(arg2, start_pos, end_pos)
+    --        print(hexValue)
+    --    end
+    --end
     --print(arg1)
     --print(arg2)
 end)
--- TimonHelper/Modules/Spells/Warrior.lua
+++ TimonHelper/Modules/Spells/Warrior.lua
@@ -1,46 +1,6 @@
 print('Warrior.lua loaded')
 
 --spells
-
---ActionBar page 1: slots 1 to 12 -- Note exceptions below for other classes
---ActionBar page 2: slots 13 to 24
---
---ActionBar page 3 (Right ActionBar): slots 25 to 36
---ActionBar page 4 (Right ActionBar 2): slots 37 to 48
---
---ActionBar page 5 (Bottom Right ActionBar): slots 49 to 60
---ActionBar page 6 (Bottom Left ActionBar): slots 61 to 72
---
---
---Warrior Bonus Action Bars
---
---ActionBar page 1 Battle Stance: slots 73 to 84
---ActionBar page 1 Defensive Stance: slots 85 to 96
---ActionBar page 1 Berserker Stance: slots 97 to 108
---
---
---Druid Bonus Action Bars
---
---ActionBar page 1 Cat Form: slots 73 to 84
---ActionBar page 1 Prowl: slots 85 to 96
---ActionBar page 1 Bear Form: slots 97 to 108
---ActionBar page 1 Moonkin Form: slots 109 to 120
---
---
---Rogue Bonus Action Bars
---
---ActionBar page 1 Stealth: slots 73 to 84
---
---
---Priest Bonus Action Bars
---
---ActionBar page 1 Shadowform: slots 73 to 84
---
---
---Target Possessed Action Bar
---
---ActionBar page 1 Possess: slots 121-132
-
 warrior_spells = {
     battle_shout = {
         action_slot = 62,
@@ -125,6 +85,26 @@ can_check_melee_range = true
 melee_range_spell = warrior_spells.rend
 
 function DoWarriorRotation()
+    if aoe_mode then
+        DoWarriorAoeRotation()
+    else
+        DoWarriorSingleRotation()
+    end
+end
+
+function DoWarriorAoeRotation()
+    TargetEnemy()
+    SetAutoAttack()
+    go_blood_fury()
+    go_battle_shout()
+    go_charge()
+    go_bloodrage()
+    go_rend()
+    go_hamstring()
+    go_heroic_strike()
+end
+
+function DoWarriorSingleRotation()
     TargetEnemy()
     SetAutoAttack()
     go_blood_fury()
